7366ff46fcf75c2f467702cd1b1ed69c7ad40d10
Shane <shanemcneil19@gmail.com>
2019-09-03 11:45:04 -0700

    add functional Bomb component

--- src/state-drills/Bomb.js
+++ src/state-drills/Bomb.js
@@ -1,2 +1,40 @@
 import React, { Component } from 'react';
 
+export default class Bomb extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: 0
+        };
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => {
+            this.setState({
+                count: this.state.count + 1
+            })
+        }, 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval)    
+    }
+
+    render() {
+        let word;
+        if(this.state.count===8) {
+            word = <p>BOOM!!!!</p>
+            clearInterval(this.interval) 
+        } else if (this.state.count%2===0) {
+            word = <p>tick</p>
+        } else {
+            word = <p>tock</p>
+        }
+        console.log(this.state.count);
+        return (
+            <div>
+                {word}
+            </div>
+        )
+    }
+}
