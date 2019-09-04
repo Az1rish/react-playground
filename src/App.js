5bdac340dbd32ed0073a32eb5cd053605e0bf0ff
Shane <shanemcneil19@gmail.com>
2019-09-03 19:56:48 -0700

    create Tabs component and import enzyme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import Split from './composition/Split';
+import React, { Component } from 'react';
+import Tabs from './state/Tabs';
+{/*import Split from './composition/Split';
 import './App.css';
 import Tooltip from './composition/Tooltip';
 import Messages from './Messages';
@@ -8,6 +9,7 @@ import Counter from './state/Counter';
 import HelloWorld from './state-drills/HelloWorld';
 import Bomb from './state-drills/Bomb'
 import RouletteGun from './state-drills/RouletteGun';
+
 
 // make 2 tooltips here and another inside the App directly
 const firstTooltip = (
@@ -19,12 +21,30 @@ const secondTooltip = (
   <Tooltip color='#126BCC' message='another tooltip message'>
     officiis
   </Tooltip>
-)
+)*/}
 
-function App() {
+// array of objects for use with the Tabs component
+const tabsProp = [
+  { name: 'First tab',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.'},
+  { name: 'Second tab',
+    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.'},
+  { name: 'Third tab',
+    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.'},
+];
+
+export default class App extends Component {
+  render() {
+    return <div>
+      <Tabs tabs={tabsProp} />
+    </div>
+  }
+}
+
+{/*function App() {
   return (
     <main className='App'>
-      {/*<Split className='left' flexBasis={2}>
+      <Split className='left' flexBasis={2}>
         That was the content for the left 'Split'.  Lorem {firstTooltip} dolor sit amet consectetur, adipisicing elit.  Incidunt ex velit suscipit facere officia?<br />
         // make another tootip directly inside the App 
         <Tooltip message='one more tooltip message'>
@@ -35,14 +55,15 @@ function App() {
         This is the content for the right 'Split'.  Inventore aliquid cupiditate suscipit repellat.  Quaerat quis {secondTooltip} quam fuga.  Aliquid quo possimus id soluta aspernatur.
   </Split>
       <Messages name='Messages' unread={0} />
-  <Messages name='Notifications' unread={10} />*/}
+  <Messages name='Notifications' unread={10} />
       <TheDate />
       {/*<Counter count={123} step={4}/>
       <HelloWorld />
-      <Bomb />*/}
+      <Bomb />
       <RouletteGun bulletInChamber={3}/>
+      <Tabs />
     </main>
   );
 }
 
-export default App;
+export default App;*/}
