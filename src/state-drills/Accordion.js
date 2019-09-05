62198ef03e964f12489d8f5726e71cad9d3146a2
Shane <shanemcneil19@gmail.com>
2019-09-05 10:23:07 -0700

    create v1 of Accordion component, not working properly

--- src/state-drills/Accordion.js
+++ src/state-drills/Accordion.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
 
 export default class Accordion extends Component {
+    static defaultProps = {
+        sections: [],
+    };
+    
+    state = {
+        currentSection: 0,
+    }
+
+    handleButtonClick(index) {
+        this.setState = {
+            currentSection: index,
+        }
+    }
+
+    renderSection() {
+        const currentSection = this.props.sections[this.state.currentSection]
+        return (
+            <p className="content">
+                {currentSection.content}
+            </p>
+        )
+    }
+
+    renderButtons() {
+        return this.props.sections.map((section, index) => (
+            <li>
+                <button key={index} onClick={() => this.handleButtonClick(index)}>
+                    {section.title} 
+                </button>   
+                
+            </li>
+        ))
+    }
     
     render() {
         
         return (
             <ul>
-
+                {this.renderButtons()}
+                {!!this.props.sections.length &&this.renderSection()} 
             </ul>
         )
     }
