3ca3fefc2bd67021bb6eda4e3c88b2c978bcd640
Shane <shanemcneil19@gmail.com>
2019-09-30 12:47:48 -0700

    setup AppLang component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './paltrows-power-toes/App';
+import AppLang from './lang-context/AppLang';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>, 
+    <AppLang />, 
     document.getElementById('root')
 );
