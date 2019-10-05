b0c35b4e8010ccfd870f8eb8314d3b11ce7e9d14
Shane <shanemcneil19@gmail.com>
2019-10-05 13:07:39 -0700

    complete validation and add ValidationError component

--- src/registrationForm/registrationForm.js
+++ src/registrationForm/registrationForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ValidationError from './ValidationError';
 
 
 export default class RegistrationForm extends Component {
@@ -6,27 +7,45 @@ export default class RegistrationForm extends Component {
         super(props);
         this.state = {
             name: {
-                value: ''
+                value: '',
+                touched: false
             },
             password: {
-                value: ''
+                value: '',
+                touched: false
             },
             repeatPassword: {
-                value: ''
+                value: '',
+                touched: false
             }
         }
     }
 
     updateName(name) {
-        this.setState({name: {value: name}});
+        this.setState({
+            name: {
+                value: name, 
+                touched: true
+            }
+        });
     }
       
     updatePassword(password) {
-        this.setState({password: {value: password}});
+        this.setState({
+            password: {
+                value: password, 
+                touched: true
+            }
+        });
     }
       
     updateRepeatPassword(repeatPassword) {
-        this.setState({repeatPassword: {value: repeatPassword}});
+        this.setState({
+            repeatPassword: {
+                value: repeatPassword, 
+                touched: true
+            }
+        });
     }
 
     handleSubmit(e) {
@@ -39,7 +58,7 @@ export default class RegistrationForm extends Component {
         //potentially submit these values to the server here
     }
 
-    validateName(fieldValue) {
+    validateName() {
         const name = this.state.name.value.trim();
         if (name.length === 0) {
           return 'Name is required';
@@ -69,6 +88,10 @@ export default class RegistrationForm extends Component {
       }
 
     render () {
+        const nameError = this.validateName();
+        const passwordError = this.validatePassword();
+        const repeatPasswordError = this.validateRepeatPassword();
+
         return (
           <form 
             className="registration"
@@ -76,21 +99,25 @@ export default class RegistrationForm extends Component {
             <h2>Register</h2>
             <div className="registration__hint">* required field</div>  
             <div className="form-group">
-              <label htmlFor="name">Name *</label>
-              <input 
-                type="text" className="registration__control"
-                name="name" 
-                id="name"
-                onChange={e => this.updateName(e.target.value)} />
+                <label htmlFor="name">Name *</label>
+                <input 
+                    type="text" className="registration__control"
+                    name="name" 
+                    id="name"
+                    onChange={e => this.updateName(e.target.value)} />
+                {this.state.name.touched && (<ValidationError message={nameError} />)}
             </div>
             <div className="form-group">
-               <label htmlFor="password">Password *</label>
-               <input 
-                type="password" className="registration__control"
-                name="password" 
-                id="password"
-                onChange={e => this.updatePassword(e.target.value)}/>
-               <div className="registration__hint">6 to 72 characters, must include a number</div>
+                <label htmlFor="password">Password *</label>
+                <input 
+                    type="password" className="registration__control"
+                    name="password" 
+                    id="password"
+                    onChange={e => this.updatePassword(e.target.value)}/>
+                <div className="registration__hint">
+                   6 to 72 characters, must include a number
+                </div>
+                {this.state.password.touched && (<ValidationError message={passwordError} />)}
             </div>
             <div className="form-group">
               <label htmlFor="repeatPassword">Repeat Password *</label>
@@ -99,15 +126,21 @@ export default class RegistrationForm extends Component {
                 name="repeatPassword" 
                 id="repeatPassword"
                 onChange={e => this.updateRepeatPassword(e.target.value)}/>
+                {this.state.repeatPassword.touched && (<ValidationError message={repeatPasswordError} />)}
             </div>
      
             <div className="registration__button__group">
              <button 
                 type="reset" className="registration__button">
-                 Cancel
+                Cancel
              </button>
              <button 
-                type="submit" className="registration__button">
+                type="submit" className="registration__button"
+                disabled={
+                    this.validateName() ||
+                    this.validatePassword() ||
+                    this.validateRepeatPassword()
+                }>
                  Save
              </button>
             </div>
